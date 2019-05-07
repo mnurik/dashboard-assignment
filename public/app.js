a81829ad3aff4866758c4a900573e5603ea1f6aa
Nurlan <nurlanmirzay@gmail.com>
2019-05-07 09:03:27 +0200

    visualized table

--- public/app.js
+++ public/app.js
@@ -11,9 +11,39 @@
   const resAppIds = await fetch('/app')
   const appIds = await resAppIds.json();
 
-  const ths = fields.map(field => `<th>${field}</th>`);
+  const resAppId = await fetch('/app/100697065')
+  const appId = await resAppId.json();
+
+  const resAppId1 = await fetch('/app/100697065/sentQuality')
+  const appId1 = await resAppId1.json();
+  const sorted = appId1.map(({ sentQuality }) => sentQuality);
+
+  const max = Math.max(...sorted);
+  const min = Math.min(...sorted);
+
+  const trs = Object.entries(appId).map(([_, values]) => {
+    const tds = fields.map(field => {
+      const value = values[field];
+      if (field === 'sentQuality' && value) {
+        const perc = (value / (max - min)) * 100;
+        const isRed = perc < 50;
+        const color = isRed ? `rgb(${Math.round((perc * 255) / 49)},0,0)` : `rgb(0,${Math.round((perc * 255) / 100)}, 0)`
+        return `<td style="background-color:${color};color:white;">${value}</td>`;
+      }
+      return `<td>${value}</td>`;
+    });
+    return `<tr>${tds.join('')}</tr>`
+  })
+
+  const options = fields.map(field => `<option value="${field}">${field}</option>`);
+  const ths = fields.map(field => `<th>
+    <select value="${field}">
+      ${options.join('').replace(`<option value="${field}">${field}</option>`, `<option value="${field}" selected>${field}</option>`)}
+    </select>
+  </th>`);
   const thead = `<thead>${ths.join('')}</thead>`;
-  const tbody = `<tbody>${}</tbody>`;
+
+  const tbody = `<tbody>${trs.join('')}</tbody>`;
 
   const table = `
                 <table>
@@ -23,6 +53,5 @@
           `;
 
   container.innerHTML = table;
-
 })()
 // });
